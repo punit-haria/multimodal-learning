6a22e80cf00792211d0b6f8344eeeb7e1ebb7a51
punit-haria <punitshah163@gmail.com>
2017-07-02 19:33:30 +0100

    fixed

--- code/data.py
+++ code/data.py
@@ -5,24 +5,6 @@ import numpy as np
 import struct
 
 
-def sample(data, batch_size):
-    '''
-    Generic sampling function with uniform distribution.
-
-    data: numpy array or list of numpy arrays
-    batch_size: sample size
-    '''
-    if not isinstance(data, list):
-        n = len(data)
-        idx = np.random.randint(len(data), size=batch_size)
-        return idx, data[idx], 
-    else:
-        n = {len(x) for x in data}
-        assert len(n) == 1
-        n = n.pop()
-        idx = np.random.randint(n, size=batch_size)
-        return idx, tuple(x[idx] for x in data)
-
 
 class DAY_NIGHT(object):
     """
@@ -31,6 +13,61 @@ class DAY_NIGHT(object):
     def __init__(self,):
         pass
 
+
+class JointMNIST(MNIST):
+    """
+    MNIST data treated as two output variables consisting of the top halves and bottom halves of 
+    each image. 
+    """
+    def __init__(self, n_paired):
+        """
+        n_paired: number of paired examples (remaining examples are split into one of top or bottom halves)
+        """
+        super(JointMNIST, self).__init__()  # load data
+        self.n_paired = n_paired  
+        self.split_point = 784 / 2  
+
+        # joint and missing split
+        self.x_and_y = set(np.arange(1000))
+        self.x_only = set(len(x_and_y) + np.arange(29500))
+        self.y_only = set(len(x_and_y) + len(x_only) + np.arange(29500))
+
+
+    def sample(self, set='train', batch_size=100, include_labels=False):
+        # sample naively
+        idx, (batch,labels) = self._sample(set, batch_size)
+
+        # handle test set case separately
+        if set == 'test':
+            X = batch[:, 0:self.split_point]
+            Y = batch[:,self.split_point:]
+            if include_labels:
+                return (X,labels), (Y,labels)
+            else:
+                return X, Y
+
+        # separate indices into paired and missing (for training set)
+        x_idx = np.array(list(set(idx) & self.x_only))
+        x_idx = np.array([np.argwhere(idx == x)[0,0]  for x in x_idx], dtype=np.int32)
+        y_idx = np.array(list(set(idx) & self.y_only))
+        y_idx = np.array([np.argwhere(idx == x)[0,0]  for x in y_idx], dtype=np.int32)
+        xy_idx = np.array(list(set(idx) & self.x_and_y))
+        xy_idx = np.array([np.argwhere(idx == x)[0,0]  for x in xy_idx], dtype=np.int32)
+
+        # create separate arrays for jointly observed and marginal data
+        X = batch[x_idx, 0:self.split_point]
+        Y = batch[y_idx, self.split_point:]
+        X_joint = batch[xy_idx, 0:self.split_point]
+        Y_joint = batch[xy_idx, self.split_point:]
+    
+        if include_labels:  # split label data too
+            lX = labels[x_idx]
+            lY = labels[y_idx]
+            l_joint = labels[xy_idx]
+            return (X, lX), (Y, lY), (X_joint, l_joint), (Y_joint, l_joint)
+
+        else:
+            return X, Y, X_joint, Y_joint
 
 
 
@@ -48,12 +85,32 @@ class MNIST(object):
         self.Xte, self.yte = _get_data(self.test_path, self.test_labels_path)
 
 
+    def sample(self, set='train', batch_size=100):
+        """
+        Samples data from training set. 
+        """
+        _, (X,Y) = self._sample(set, batch_size)
+        return X, Y
+
+
     def train_set(self,):
         return self.Xtr, self.ytr
 
     
     def test_set(self,):
         return self.Xte, self.yte
+
+
+    def _sample(self, set='train', batch_size=100):
+        """
+        Samples data from training set. 
+        """
+        if set == 'train':
+            return sample([self.Xtr, self.ytr], batch_size)
+        elif set == 'test':
+            return sample([self.Xte, self.yte], batch_size)
+        else:
+            raise Exception('Training or test set not selected..')
 
 
     def _get_data(data_path, labels_path):
@@ -82,3 +139,21 @@ class MNIST(object):
             return data.reshape(shape)
         
 
+
+def sample(data, batch_size):
+    '''
+    Generic sampling function with uniform distribution.
+
+    data: numpy array or list of numpy arrays
+    batch_size: sample size
+    '''
+    if not isinstance(data, list):
+        n = len(data)
+        idx = np.random.randint(len(data), size=batch_size)
+        return idx, data[idx], 
+    else:
+        n = {len(x) for x in data}
+        assert len(n) == 1
+        n = n.pop()
+        idx = np.random.randint(n, size=batch_size)
+        return idx, tuple(x[idx] for x in data)
--- code/experiment_joint_vae.py
+++ code/experiment_joint_vae.py
@@ -9,65 +9,40 @@ import utils
 from models.joint_vae import JointVAE
 
 
-# parameters
+### PARAMETERS ###
 learning_rate = 0.002                           
 batch_size = 250  
+z_dim = 50                
+n_hidden = 200
+n_paired = 1000
+
 x_dim = 392 
 y_dim = 392                   
-z_dim = 50                    
 train_steps = 10000
 
 
-# joint/missing split
-x_and_y = set(np.arange(1000))
-x_only = set(len(x_and_y) + np.arange(29500))
-y_only = set(len(x_and_y) + len(x_only) + np.arange(29500))
 
+# data set
+mnist = data.JointMNIST(n_paired)
 
-# train/test sets
-mnist = data.MNIST()
-Xtr, ytr = mnist.training_set()
-Xte, yte = mnist.testing_set()
-
-
-# joint variational auto-encoder
-model_name = 'vae_lr_'+str(learning_rate)+'_batch_'+str(batch_size)
-vae = JointVAE((x_dim, y_dim), z_dim, learning_rate, name=model_name)
+# model
+vae = JointVAE((x_dim, y_dim), z_dim, learning_rate, n_hidden, name='joint_vae')
 
 
 # train model
 for i in range(train_steps):
     
-    # randomly sampled minibatch 
-    idx, batch = data.sample(Xtr, batch_size)
-
-    # separate indices
-    x_idx = np.array(list(set(idx) & x_only))
-    x_idx = np.array([np.argwhere(idx == x)[0,0]  for x in x_idx], dtype=np.int32)
-    y_idx = np.array(list(set(idx) & y_only))
-    y_idx = np.array([np.argwhere(idx == x)[0,0]  for x in y_idx], dtype=np.int32)
-    xy_idx = np.array(list(set(idx) & x_and_y))
-    xy_idx = np.array([np.argwhere(idx == x)[0,0]  for x in xy_idx], dtype=np.int32)
-
-    # separate jointly observed and missing data
-    X = batch[x_idx, 0:x_dim]
-    Y = batch[y_idx, x_dim:]
-    X_joint = batch[xy_idx, 0:x_dim]
-    Y_joint = batch[xy_idx, x_dim:]
+    # random minibatch 
+    X, Y, X_joint, Y_joint = mnist.sample('train', batch_size)
 
     # training step
     vae.train(X, Y, X_joint, Y_joint)
-
 
     if i % 25 == 0:
         print("At iteration ", i)
 
         # test minibatch
-        idx, test_batch = data.sample(Xte, 1000)
-
-        # test solely on joint data
-        X = test_batch[:,0:x_dim]
-        Y = test_batch[:,x_dim:]
+        X, Y = mnist.sample('test', 1000)
 
         # test model
         vae.test(X, Y, X, Y)
