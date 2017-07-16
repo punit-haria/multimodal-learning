4191345077807b038d3f09959987f4a65004220e
punit-haria <punitshah163@gmail.com>
2017-07-16 16:00:29 +0100

    update

---
+++ code/models/networks.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import numpy as np
+
+
+
+def pixel_cnn(x, args, scope, reuse):
+    """
+    PixelCNN network based on architectures specified in:
+        https://arxiv.org/abs/1601.06759
+        https://arxiv.org/abs/1701.05517
+
+    x: input tensor
+    args: dictionary with parameters --> n_layers, image_dim, filter_w
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+
+        n_layers = args['n_layers']
+        image_dim = args['image_dim']
+        k = args['filter_w']
+
+        x_image = tf.reshape(x, shape=image_dim)
+
+        c = conv2d_masked(x_image, 3, 10, 'A', 'layer_1', reuse)
+
+        for i in range(n_layers):
+            name  = 'layer_' + str(i+2)
+            c = conv2d_masked(c, 3, 10, 'B', name, reuse)
+            c = tf.nn.relu(c)
+
+        return c
+
+
+def conv2d_masked(x, k, out_ch, mask_type, scope, reuse):
+    """
+    Masked 2D convolution
+
+    x: input tensor
+    k: convolution window
+    out_ch: number of output channels
+    mask_type: mask type 'A' or 'B' (see https://arxiv.org/abs/1601.06759)
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+
+        assert k % 2 == 1  # check that k is odd
+        in_ch = x.get_shape()[3].value  # number of input channels
+
+        w = tf.get_variable("W", shape=[k, k, in_ch, out_ch],
+                            initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+
+        # create mask
+        mask = np.zeros(shape=[k, k, in_ch, out_ch])
+        half_k = (k // 2) + 1
+        for i in range(half_k):
+            for j in range(k):
+                if i < half_k - 1 or j < half_k - 1:
+                    mask[i,j,:,:] = 1
+
+        # mask type
+        if mask_type == 'A':
+            mask[half_k-1,half_k-1,:,:] = 0
+        elif mask_type == 'B':
+            mask[half_k - 1, half_k - 1, :, :] = 1
+        else:
+            raise Exception("Masking type not implemented..")
+
+        # mask filter and apply convolution
+        w_masked = tf.multiply(w, tf.constant(mask))
+        c = tf.nn.conv2d(x, w_masked, strides=[1,1,1,1], padding='SAME')
+
+        return c
+
+
+
+def linear(x, n_x, n_w, scope, reuse):
+    """
+    Linear tranform
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        w = tf.get_variable("W", shape=[n_x, n_w],
+                            initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+        b = tf.get_variable("b", shape=[n_w], initializer=tf.constant_initializer(0.1))
+
+        return tf.matmul(x, w) + b
+
+
+
+
+
+
--- code/present_coloured.py
+++ code/present_coloured.py
@@ -12,7 +12,7 @@ res = Results.load(experiment)
 
 ### Figure 1 ###
 
-trials = ['vae_joint', 'vae_translate']
+trials = ['mnist_colored_vae_joint', 'mnist_colored_vae_translate']
 labels = ['Joint Bound', 'Translation Bound']
 
 plt.figure(figsize=(12,9))
@@ -22,7 +22,7 @@ for i,t in enumerate(trials):
     steps, series = run.get_series('test_lower_bound')
     plt.plot(steps, series, label=labels[i], linewidth=2)
 
-plt.axis([0,10000,-200,-100])
+plt.axis([0,5000,-700,-550])
 plt.legend(loc='lower right', fontsize=18)
 plt.xlabel('Training Steps')
 plt.ylabel('Log-Likelihood Lower Bound')
@@ -35,7 +35,7 @@ plt.close('all')
 
 ### Figure 2 ###
 
-trials = ['vae_joint', 'vae_translate']
+trials = ['mnist_colored_vae_joint', 'mnist_colored_vae_translate']
 labels = ['Coloured Joint Bound', 'Coloured Translation Bound']
 
 n_images = 18
@@ -53,7 +53,7 @@ for time_step in time_steps:
         x1b, rx2_1 = run.get_series('x2_1', i=time_step)
 
         images = np.concatenate((x1b[0:n_images],rx2_1[0:n_images]), axis=0)
-        images = np.reshape(images, [-1,28,28])
+        images = np.reshape(images, [-1,28,28,3])
 
         plot.plot_images(images, 6, 6, '../plots/'+labels[i]+'_reconstruct_from_x1_'+str(time_step)+'.png')
 
@@ -62,7 +62,7 @@ for time_step in time_steps:
         x2b, rx1_2 = run.get_series('x1_2', i=time_step)
 
         images = np.concatenate((x2b[0:n_images],rx1_2[0:n_images]), axis=0)
-        images = np.reshape(images, [-1,28,28])
+        images = np.reshape(images, [-1,28,28,3])
 
         plot.plot_images(images, 6, 6, '../plots/'+labels[i]+'_reconstruct_from_x2_'+str(time_step)+'.png')
 
