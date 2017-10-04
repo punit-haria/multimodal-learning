189871f8dd9360f0837990e379e9826b09f125f0
punit-haria <punitshah163@gmail.com>
2017-10-04 16:36:19 -0700

    update

--- code/runcoco.py
+++ code/runcoco.py
@@ -4,7 +4,7 @@ from data import MSCOCO
 from training import train_joint, Results
 
 
-experiment_name = 'mscoco_initial'
+experiment_name = 'coco'
 
 
 models = [
@@ -15,29 +15,31 @@ models = {x.__name__: x for x in models}
 
 # parameters
 parms = {
-    # options
-    'objective': 'joint',    # joint, translate
-
     # basic parameters
-    'n_z': 200,
+    'objective': 'joint',    # joint, translate
+    'n_z': 50,
 
     # network parameters
-    'n_units': 96,
-    'n_feature_maps': 32,  # 32
+    'n_feature_maps': 32,
+    'n_units': 80,
+    'embed_size': 80,
+    'max_seq_len': 10,   ############## ?????
+    'vocab_size': 240,   ############## ?????
+    'gru_layers': 1,
+
 
     # loss function parameters
-    'anneal': -0.25,  # 0, -0.0625, -0.125, -0.25
-    'joint_anneal': 0.3,   # 0.1, 0.3, 0.5
+    'anneal': 0,  # 0, -0.0625, -0.125, -0.25   (0 is no anneal)
+    'joint_anneal': 0.3,   # 0.1, 0.3, 0.5      (1 is no anneal)
 
     # train/test parameters
     'learning_rate': 0.001,
-    'n_unpaired_samples': 256,
-    'n_paired_samples': 64,
+    'n_unpaired_samples': 128,
+    'n_paired_samples': 128,
 
-    'n_paired': 2000,
-    'n_conditional_pixels': 0,
-    'test_sample_size': 256,
-    'train_steps': 10000,
+    'n_paired': 40000,
+    'test_sample_size': 128,
+    'train_steps': 10000,  # 350000
     'test_steps': 50,
     'save_steps': 2000
 }
@@ -45,67 +47,41 @@ parms = {
 
 if __name__ == "__main__":
 
+    # objective, n_z, n_feature_maps, n_units, embed_size, gru_layers, anneal, joint_anneal, learning_rate
 
-
-
-
-
-    # data, type, flow, flow_layers, flow_units, flow_type, autoregressive, n_ar_layers, anneal,
-    # joint_type, n_z, n_mix, lr, n_units, n_fmaps, objective, joint_anneal
 
     configs = [
-        ["cnn", "discrete", False, 4, 1024, "made", False, 2, 0, 'small', 64, 3, 0.001, 128, 32, 'joint', 0.3]
+        ['joint', 50, 32, 80, 80, 1, 0, 0.3, 0.001]
     ]
 
-    data = ColouredStratifiedMNIST(parms['n_paired'])
+    data = MSCOCO(parms['n_paired'])
 
     tracker = Results(experiment_name)  # performance tracker
 
     for c in configs:
-        parms['type'] = c[0]
-        parms['output'] = c[1]
+        parms['objective'] = c[0]
+        parms['n_z'] = c[1]
 
-        parms['flow'] = c[2]
-        parms['flow_layers'] = c[3]
-        parms['flow_units'] = c[4]
-        parms['flow_type'] = c[5]
+        parms['n_feature_maps'] = c[2]
+        parms['n_units'] = c[3]
+        parms['embed_size'] = c[4]
+        parms['gru_layers'] = c[5]
 
-        parms['autoregressive'] = c[6]
-        parms['n_pixelcnn_layers'] = c[7]
+        parms['anneal'] = c[6]
+        parms['joint_anneal'] = c[7]
 
-        parms['anneal'] = c[8]
-
-        parms['joint_type'] = c[9]
-
-        parms['n_z'] = c[10]
-        parms['n_mixtures'] = c[11]
-        parms['learning_rate'] = c[12]
-        parms['n_units'] = c[13]
-        parms['n_feature_maps'] = c[14]
-
-        parms['objective'] = c[15]
-
-        parms['joint_anneal'] = c[16]
+        parms['learning_rate'] = c[8]
 
 
         for name, model in models.items():
 
-            name =  experiment_name + '_' + parms['type'] + '_' + parms['joint_type'] \
+            name =  experiment_name + '_obj_' + parms['objective'] \
                     + '_nz_' +  str(parms['n_z']) \
                     + '_lr_' + str(parms['learning_rate']) \
                     + '_fmaps_' + str(parms['n_feature_maps']) \
                     + '_units_' + str(parms['n_units']) \
-                    + '_obj_' + parms['objective']
-
-            if parms['output'] == "continuous":
-                name += '_nmix_' + str(parms['n_mixtures'])
-
-
-            if parms['flow']:
-                name += "_flow_" + str(parms['flow_layers']) + "_" + str(parms['flow_units']) + "_" + parms['flow_type']
-
-            if parms['autoregressive']:
-                name += "_ar_" + str(parms['n_pixelcnn_layers'])
+                    + '_embed_' + str(parms['embed_size']) \
+                    + '_gru_' + str(parms['gru_layers'])
 
             if parms['anneal'] < 0:
                 name += "_anneal_" + str(parms['anneal'])
