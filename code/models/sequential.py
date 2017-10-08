5c0ea4fe321c079ab384b06807207497c07710ca
punit-haria <punitshah163@gmail.com>
2017-10-07 17:28:24 -0700

    update

--- code/models/sequential.py
+++ code/models/sequential.py
@@ -14,47 +14,12 @@ class GRUCell(tf.contrib.rnn.RNNCell):
         self._activation = activation
         self._init = init
 
-        '''
-        with tf.variable_scope('rnn'):
-            with tf.variable_scope('multi_rnn_cell'):
-                with tf.variable_scope('cell_0'):
-                    with tf.variable_scope('gru_cell'):
-                        with tf.variable_scope('gates'):
-                            with tf.variable_scope('rt_zt'):
-                                pass
-
-                        with tf.variable_scope('candidate'):
-                            with tf.variable_scope('ct'):
-                                pass
-        '''
         if self._init:
             with tf.variable_scope('rnn/multi_rnn_cell/cell_0/gru_cell/gates/rt_zt'):
                 self._initialize_variables(input, 2 * self._num_units)
 
             with tf.variable_scope('rnn/multi_rnn_cell/cell_0/gru_cell/candidate/ct'):
                 self._initialize_variables(input, self._num_units)
-
-
-    def _initialize_variables(self, inputs, n_out):
-
-        # inputs: batch x time x depth
-        inp = tf.slice(inputs, begin=[0,0,0], size=[-1,1,-1])
-        inp = tf.squeeze(inp)   # batch x depth
-        state = tf.zeros(shape=[tf.shape(inp)[0], self._num_units])
-        inp = tf.concat([inp, state], axis=1)
-
-        n_x = inp.get_shape()[1].value
-
-        v = tf.get_variable("v", shape=[n_x, n_out], initializer=tf.random_normal_initializer(0, 0.05))
-        v_norm = tf.nn.l2_normalize(v.initialized_value(), dim=0)
-
-        t = tf.matmul(inp, v_norm)
-        mu_t, var_t = tf.nn.moments(t, axes=0)
-
-        inv = 1 / tf.sqrt(var_t + 1e-10)
-
-        _ = tf.get_variable("g", initializer=inv)
-        _ = tf.get_variable("b", initializer=-mu_t * inv)
 
 
     @property
@@ -85,6 +50,28 @@ class GRUCell(tf.contrib.rnn.RNNCell):
         return new_h, new_h
 
 
+    def _initialize_variables(self, inputs, n_out):
+
+        # inputs: batch x time x depth
+        inp = tf.slice(inputs, begin=[0,0,0], size=[-1,1,-1])
+        inp = tf.squeeze(inp)   # batch x depth
+        state = tf.zeros(shape=[tf.shape(inp)[0], self._num_units])
+        inp = tf.concat([inp, state], axis=1)
+
+        n_x = inp.get_shape()[1].value
+
+        v = tf.get_variable("v", shape=[n_x, n_out], initializer=tf.random_normal_initializer(0, 0.05))
+        v_norm = tf.nn.l2_normalize(v.initialized_value(), dim=0)
+
+        t = tf.matmul(inp, v_norm)
+        mu_t, var_t = tf.nn.moments(t, axes=0)
+
+        inv = 1 / tf.sqrt(var_t + 1e-10)
+
+        _ = tf.get_variable("g", initializer=inv)
+        _ = tf.get_variable("b", initializer=-mu_t * inv)
+
+
     def gru_linear(self, args, n_out, init, scope):
 
         total_arg_size = 0
@@ -96,30 +83,10 @@ class GRUCell(tf.contrib.rnn.RNNCell):
 
             scope.reuse_variables()
 
-            print("-------------------------------", tf.get_variable_scope().name, flush=True)
-            print("Init:", init, flush=True)
-
             n_x = total_arg_size
             x = tf.concat(args, axis=1)
 
             if init:
-                '''
-                v = tf.get_variable("v", shape=[n_x, n_out], initializer=tf.random_normal_initializer(0, 0.05))
-                v_norm = tf.nn.l2_normalize(v.initialized_value(), dim=0)
-
-                t = tf.matmul(x, v_norm)
-                mu_t, var_t = tf.nn.moments(t, axes=0)
-
-                inv = 1 / tf.sqrt(var_t + 1e-10)
-
-                _ = tf.get_variable("g", initializer=inv)
-                _ = tf.get_variable("b", initializer=-mu_t * inv)
-
-                inv = tf.reshape(inv, shape=[1, n_out])
-                mu_t = tf.reshape(mu_t, shape=[1, n_out])
-
-                return tf.multiply(t - mu_t, inv)
-                '''
                 v = tf.get_variable("v", shape=[n_x, n_out])
                 v_norm = tf.nn.l2_normalize(v.initialized_value(), dim=0)
 
