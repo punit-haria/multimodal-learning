80abd9c9ddccaf13c917819119864cd30155e8ce
punit-haria <punitshah163@gmail.com>
2017-10-18 19:16:19 -0700

    update

--- code/models/layers.py
+++ code/models/layers.py
@@ -80,7 +80,7 @@ def seq_decoder_cnn(z, x_dec, n_units, vocab_size, embed_size, init, scope):
     """
     with tf.variable_scope(scope):
         nonlin = tf.nn.elu
-        n_layers = 6
+        n_layers = 7
         n_fmaps = n_units
 
         if init:
@@ -102,11 +102,15 @@ def seq_decoder_cnn(z, x_dec, n_units, vocab_size, embed_size, init, scope):
 
         # dilated convoluations as residual blocks:
 
-        c = conv1d(input, k=3, out_ch=n_fmaps, dilation=2, mask_type='A', init=init, scope='conv1d_0')
+        dilate = 1
+        c = conv1d(input, k=3, out_ch=n_fmaps, dilation=dilate, mask_type='A', init=init, scope='conv1d_0')
 
+        dilate = 2
         for i in range(n_layers):
             scp = 'resblock_' + str(i+1)
-            c = resblock1d(c, k=3, nonlinearity=nonlin, dilation=2, init=init, scope=scp)
+            dilate = 1 if dilate > 16 else dilate
+            c = resblock1d(c, k=3, nonlinearity=nonlin, dilation=dilate, init=init, scope=scp)
+            dilate *= 2
 
         c = nonlin(c)
         c = conv1d(c, k=1, out_ch=n_fmaps, dilation=2, mask_type='B', init=init, scope='final_conv1d_a')
